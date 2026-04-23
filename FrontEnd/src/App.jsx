4cd98faa4c9bdc64d55fd187b01086f77869f812
Tesnim hed <tesnissam@gmail.com>
2026-04-23 22:28:58 +0100

    fix bug

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={"Home"}/>
         <Route path="/signup" element={<SignUpPage />}/>
-        <Route path="/login" element={<LoginUpPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
       </Routes>
 
     </div>
--- FrontEnd/src/Pages/SignUpPage.jsx
+++ FrontEnd/src/Pages/SignUpPage.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { motion } from "framer-motion"
-import input from '../components/input'
+import Input from '../components/input'
 import { User, Mail, Lock } from 'lucide-react'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function SignUpPage() {
     const [name, setName] = useState("") //etat enitial de name est vide
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const HundleSignUp = (e) => {
+    const HandleSignUp = (e) => {
         e.preventDefault()
     }
     return (
         <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl  rounded-2xl shadow-xl overflow-hidden'>
             <div className='p-8'>
                 <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>Create account</h2>
-                <form onSubmit={HandleSignUp()}>
+                <form onSubmit={HandleSignUp}>
                     <Input icon={User} type="text" placeholder="Username" value={name} onChange={(e) => setName(e.target.value)} />
                     <Input icon={Mail} type="email" placeholder="Email Adress" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <Input icon={Lock} type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
